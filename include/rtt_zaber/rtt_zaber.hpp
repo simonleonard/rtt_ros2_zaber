8f8df9a0118ab0d2b0f08d6813ebf90e40a1ad16
Simon Leonard <sleonard@jhu.edu>
2022-08-01 22:03:45 -0400

    pranav commit

--- include/rtt_zaber/rtt_zaber.hpp
+++ include/rtt_zaber/rtt_zaber.hpp
@@ -1,17 +1,37 @@
+#include <stdexcept>
 #include <rtt/TaskContext.hpp>
 #include <rtt/InputPort.hpp>
-
 #include <rtt_ros2_node/rtt_ros2_node.hpp>
 #include <geometry_msgs/msg/wrench_stamped.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
+#include <std_msgs/msg/bool.hpp>
+
+#include <zaber/motion/ascii.h>
+using namespace zaber::motion;
+using namespace zaber::motion::ascii;
+
 
 class rtt_zaber : public RTT::TaskContext{
 
 private:
 
   RTT::InputPort<geometry_msgs::msg::WrenchStamped> port_input_wrench;
-  RTT::OutputPort<sensor_msgs::msg::JointState> port_output_jointstate;
+  RTT::InputPort<geometry_msgs::msg::Twist> port_input_teleop;
+  // RTT::OutputPort<sensor_msgs::msg::JointState> port_output_jointstate; 
+  // RTT::InputPort<std_msgs::msg::Bool> port_emergency_stop; 
+  double old_time = 0; 
+  double new_time = 0; 
+  double teleop_status = false; 
+  double teleop_vel = 0.0;
 
+  // auto nh = rclcpp::Node; 
+
+  int zaber_axis = 0;
+
+  Axis axis;
+  Device device;
+  Connection connection;
 public:
 
   rtt_zaber( const std::string& name );
@@ -23,5 +43,14 @@ public:
 
   virtual void stopHook();
   virtual void cleanupHook();
-  
+
+  void move_relatvie(const double& distance, const double& velocity);
+  void home(); 
+  void move_absolute(const double& distance, const double& velocity);
+  void move_velocity(const double& velocity);
+  void stop_axis();
+  void move_max(const double& velocity);
+  void move_min(const double& velocity);
+  void teleop_start();
+  void teleop_stop(); 
 };
--- src/rtt_zaber.cpp
+++ src/rtt_zaber.cpp
@@ -5,7 +5,10 @@
 
 rtt_zaber::rtt_zaber( const std::string& name ) :
   RTT::TaskContext( name ),
-  port_input_wrench( "wrench" ){
+  port_input_wrench( "wrench" ),
+  port_input_teleop("teleop_control")
+  // port_emergency_stop( "emergency_stop" )
+{
 
   RTT::Service::shared_ptr global_ros =
     RTT::internal::GlobalService::Instance()->getService("ros");
@@ -13,17 +16,50 @@ rtt_zaber::rtt_zaber( const std::string& name ) :
     global_ros->getOperation("create_node");
   create_node.ready();
   create_node();
+
+  // std::cout << "Time: " << global_ros::Clock::now(); std::endl;
   
-  addPort( "wrench", port_input_wrench );
+  addPort("wrench", port_input_wrench );
+  addPort("teleop_control",port_input_teleop);
+  // addPort("emergency_stop", port_emergency_stop);
+
+  addOperation("Home", &rtt_zaber::home, this, RTT::OwnThread);
+  addOperation("MoveRelative", &rtt_zaber::move_relatvie, this, RTT::OwnThread);
+  addOperation("MoveAbsolute", &rtt_zaber::move_absolute, this, RTT::OwnThread);
+  addOperation("MoveVelocity", &rtt_zaber::move_velocity, this, RTT::OwnThread);
+  addOperation("StopAxis", &rtt_zaber::stop_axis, this, RTT::OwnThread);
+  addOperation("MoveMax", &rtt_zaber::move_max, this, RTT::OwnThread);
+  addOperation("MoveMin", &rtt_zaber::move_min, this, RTT::OwnThread);
+  addOperation("TeleopStart", &rtt_zaber::teleop_start, this, RTT::OwnThread);
+  addOperation("TeleopStop", &rtt_zaber::teleop_stop, this, RTT::OwnThread);
+  
+  addProperty("zaber_axis", zaber_axis);
+
+  Library::enableDeviceDbStore();
   
 }
 
 bool rtt_zaber::configureHook(){
-  std::cout << "configureHook" << std::endl;
+  std::cout << "configureHook_1" << std::endl;
+
+  connection = Connection::openSerialPort("/dev/ttyUSB1");
+
+  std::vector<Device> deviceList = connection.detectDevices();
+  std::cout << "Found " << deviceList.size() << " devices." << std::endl;
+
+  device = deviceList[0];
+
+  axis = device.getAxis(1);
+  axis.home();
+
   return true;
 }
+
 bool rtt_zaber::startHook(){
   std::cout << "startupHook" << std::endl;
+  std::cout << "Zaber Axis: " << zaber_axis << std::endl;
+  // old_time = rclcpp::Clock::Clock::now();
+  std::cout << "Time: " << old_time << std::endl;
   return true;
 }
 
@@ -31,16 +67,134 @@ void rtt_zaber::updateHook(){
   geometry_msgs::msg::WrenchStamped wrench;
 
   if( port_input_wrench.read( wrench ) == RTT::NewData ){
-	std::cout << wrench.wrench.force.x << std::endl;    
-  }
+    std::cout << wrench.header.stamp.nanosec << std::endl;
 
+    if (wrench.wrench.force.z  > 5.0 ){
+      axis.stop();
+      throw std::invalid_argument("Force exceeded over the limit of 5N");
+    }
+        
+  }
+  
+  // geometry_msgs::msg::Twist teleop_cmd;
+    
+  // if(port_input_teleop.read(teleop_cmd) == RTT::NewData){
+  //   std::cout << "Teleop input: " << teleop_cmd.linear.x << std::endl;
+  // }
+
+  if (teleop_status)
+  {
+      geometry_msgs::msg::Twist teleop_cmd;
+      // std::cout << rclcpp::Clock::  clock::now() << std::endl;
+      if(port_input_teleop.read(teleop_cmd) == RTT::NewData){
+        if (zaber_axis == 1){
+          teleop_vel = teleop_cmd.linear.x;
+        }
+        else if (zaber_axis == 2){
+          teleop_vel = teleop_cmd.linear.y;
+        }
+        else if (zaber_axis == 3){
+          teleop_vel = teleop_cmd.linear.z;
+        }
+        else {
+          throw std::invalid_argument("Zaber axis out of bound!");
+        }
+
+        axis.moveVelocity(((50*teleop_vel)), Units::VELOCITY_MILLIMETRES_PER_SECOND);
+      }
+      else {
+        axis.moveVelocity(0, Units::VELOCITY_MILLIMETRES_PER_SECOND);
+      }
+  }
 }
 
 void rtt_zaber::stopHook(){
   std::cout << "stopHook" << std::endl;
+  axis.stop(); 
 }
 void rtt_zaber::cleanupHook(){
   std::cout << "cleanupHook" << std::endl;
+  axis.home();
 }
+
+void rtt_zaber::home()
+{
+  if (axis.isBusy()){
+    throw std::invalid_argument("Device is busy, cannot recieve new command");
+  }
+  else{
+    axis.home(false); 
+  }
+}
+
+void rtt_zaber::move_relatvie(const double& distance, const double& velocity)
+{
+  // std::cout << distance << std::endl;
+  if (axis.isBusy()){
+    throw std::invalid_argument("Device is busy, cannot recieve new command");
+  }
+  else{
+    axis.moveRelative(distance, Units::LENGTH_MILLIMETRES, false, velocity, Units::VELOCITY_MILLIMETRES_PER_SECOND);
+  }
+}
+
+void rtt_zaber::move_absolute(const double& distance, const double& velocity)
+{
+  // std::cout << distance << std::endl;
+  if (axis.isBusy()){
+    throw std::invalid_argument("Device is busy, cannot recieve new command");
+  }
+  else{
+    axis.moveAbsolute(distance, Units::LENGTH_MILLIMETRES, false, velocity, Units::VELOCITY_MILLIMETRES_PER_SECOND);
+  }
+}
+
+void rtt_zaber::move_velocity(const double& velocity)
+{
+  if (axis.isBusy()){
+    throw std::invalid_argument("Device is busy, cannot recieve new command");
+  }
+  else{
+    axis.moveVelocity(velocity, Units::VELOCITY_MILLIMETRES_PER_SECOND);
+  }
+}
+
+void rtt_zaber::stop_axis(){
+  axis.stop(false);
+}
+
+void rtt_zaber::move_max(const double& velocity)
+{
+  if (axis.isBusy()){
+    throw std::invalid_argument("Device is busy, cannot recieve new command");
+  }
+  else{
+    axis.moveMax(false, velocity, Units::VELOCITY_MILLIMETRES_PER_SECOND);
+  }
+}
+
+void rtt_zaber::move_min(const double& velocity)
+{
+  if (axis.isBusy()){
+    throw std::invalid_argument("Device is busy, cannot recieve new command");
+  }
+  else{
+    axis.moveMin(false, velocity, Units::VELOCITY_MILLIMETRES_PER_SECOND);
+  }
+}
+
+void rtt_zaber::teleop_start()
+{
+  teleop_status = true ;
+  std::cout << "Teleop control Activated!" << std::endl;
+}
+
+void rtt_zaber::teleop_stop()
+{
+  teleop_status = false; 
+  std::cout << "Teleop control Deactivated!" << std::endl;
+
+}
+
 
 ORO_CREATE_COMPONENT(rtt_zaber)
